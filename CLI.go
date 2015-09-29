8dab15265f84ec28e0af9ca676054a0b35318ee3
dhvogel <dhvogel2468@gmail.com>
2015-09-29 11:27:08 -0400

    recursive json working

--- CLI.go
+++ CLI.go
@@ -28,7 +28,7 @@ type File struct {
     //LinksTo         bool        `json:"LinksTo"`
     Size            int64       `json:"Size"`
     Name            string      `json:"Name"`
-    //Children        []File      `json:"File"` 
+    Children        []File      `json:"Children"` 
 }
 
 var config *Config
@@ -55,17 +55,17 @@ func init() {
 
 func walkFiles(path string, file os.FileInfo, err error) error {
     if strings.ToUpper(config.Output) == "TEXT" {
-        iterateFiles(path, file)
+        iterateFilesText(path, file)
     } else if strings.ToUpper(config.Output) == "JSON" {
-        printJSONFile(path, file)
+        iterateFilesJSON(path, file)
     } else if strings.ToUpper(config.Output) == "YAML" {
-        printYAMLFile(path, file)
+        iterateFilesYAML(path, file)
     }
     return nil
 }
 
 //Cannot have '/' in filesystem (other than to separate directories). Or else this will get messed up
-func iterateFiles(path string, file os.FileInfo) {
+func iterateFilesText(path string, file os.FileInfo) {
     for i:=0; i<len(strings.Split(path, "/")); i++ {
         fmt.Print(" ")
     }
@@ -83,44 +83,95 @@ func iterateFiles(path string, file os.FileInfo) {
     fmt.Print("\n")
 }
 
+
+func iterateFilesJSON(path string, file os.FileInfo) {
+    // JSONFile := &File{ModifiedTime: file.ModTime(), 
+    //                   IsDir: file.IsDir(),
+    //                   IsLink: file.Mode()&os.ModeSymlink == os.ModeSymlink, 
+    //                   Size: file.Size(), 
+    //                   Name: file.Name()}
+
+}
+
+func iterateFilesYAML(path string, file os.FileInfo) {
+
+}
+
+
 func recurseFiles(path string, depth int) {
-    var dir bool
-    depth++;
+    var files []os.FileInfo
+    files, depth = getDir(path, depth)
+    if strings.ToUpper(config.Output) == "TEXT" {
+        printText(files, depth, path)
+    } else if strings.ToUpper(config.Output) == "JSON" {
+        JSONFiles:= getJSON(files, path, depth)
+        for i:=0; i<len(JSONFiles); i++ {
+            jsonOutput, _ := json.Marshal(JSONFiles[i])
+            fmt.Println(string(jsonOutput))
+        }     
+    } else if strings.ToUpper(config.Output) == "YAML" {
+        //print(files, depth)
+    }
+
+}
+
+func getDir(path string, depth int) ([]os.FileInfo, int) {
+    depth++
     Files, _ := ioutil.ReadDir(path)
-    for i:=0; i<len(Files); i++ {
+    return Files, depth
+}
+
+func printText(files []os.FileInfo, depth int, path string) {
+    var dir bool
+    for i:=0; i<len(files); i++ {
         dir = false
         for j:=0; j<depth; j++ {
             fmt.Print(" ");
         }
-        fmt.Print(Files[i].Name())
-        if (Files[i].IsDir()) {
+        fmt.Print(files[i].Name())
+        if (files[i].IsDir()) {
             fmt.Print("/")
             dir = true
         }
-        if Files[i].Mode()&os.ModeSymlink == os.ModeSymlink {
+        if files[i].Mode()&os.ModeSymlink == os.ModeSymlink {
             fmt.Print("* (symlink)")
         }
         fmt.Print("\n")
-        if (dir == true) {  
-            recurseFiles(path + "/" + Files[i].Name(), depth)
+        if (dir == true) {
+            files, depth := getDir(path, depth)
+            printText(files,depth, path + "/" + files[i].Name())
         }
     }
 }
 
-func printJSONFile(path string, file os.FileInfo) {
-    JSONFile := &File{ModifiedTime: file.ModTime(), 
-                      IsDir: file.IsDir(),
-                      IsLink: file.Mode()&os.ModeSymlink == os.ModeSymlink, 
-                      Size: file.Size(), 
-                      Name: file.Name()}
-    jsonOutput, _ := json.Marshal(JSONFile)
-    fmt.Println(string(jsonOutput))
+func getJSON(files []os.FileInfo, path string, depth int) []File {
+    var Children []File
+    var JSONFiles []File
+    for i:=0; i<len(files);i++ {
+        if (files[i].IsDir()) {
+            DirEntries, depth := getDir(path + "/" + files[i].Name(), depth)
+            Children = getJSON(DirEntries, path + "/" + files[i].Name(), depth)
+        }
+        JSONFile := File{ModifiedTime: files[i].ModTime(), 
+                      IsDir: files[i].IsDir(),
+                      IsLink: files[i].Mode()&os.ModeSymlink == os.ModeSymlink, 
+                      Size: files[i].Size(), 
+                      Name: files[i].Name(),
+                    Children: Children}
+        JSONFiles = append(JSONFiles, JSONFile)
+    }
+    return JSONFiles
 }
 
-func printYAMLFile(path string, file os.FileInfo) {
+func recurseFilesJSON(path string) {}
+//     JSONFile := &File{ModifiedTime: file.ModTime(), 
+//                       IsDir: file.IsDir(),
+//                       IsLink: file.Mode()&os.ModeSymlink == os.ModeSymlink, 
+//                       Size: file.Size(), 
+//                       Name: file.Name()}
+// }
 
-}
-
+func recurseFilesYAML(path string){}
 
 
 
@@ -139,5 +190,6 @@ func main() {
     } else {
         recurseFiles(root, 0)
     }
+    
 
 }
